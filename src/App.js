de328e04ca7fa9ea9cc747c658b4c024b234fb2d
Deekshay22 <deekshayadav4422@gmail.com>
2026-03-05 02:51:47 +0530

    setup done

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import { Fragment } from 'react';
+import {  Button } from "reactstrap";
+
+function App() {
+  return (
+    <Fragment>
+     
+     <h1>This is bootstrao Component from react bootstrap</h1>
+      <Button color="primary" outline>First React Button</Button>
+
+
+    </Fragment>
+  );
+}
+
+export default App;
